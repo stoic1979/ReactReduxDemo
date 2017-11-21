919ea06dec8fdcb9a7c9677f1508195a2c16c68e
Navjot Singh <weavebytes@gmail.com>
2017-11-21 13:18:08 +0530

    adding redux - store, reducers, containers and actions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import allReducers from './reducers';
+import {createStore, applyMiddleware} from 'redux';
+
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    allReducers
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
